bb8b5258c8de31c06ddb1418f3be77486baff2cd
sakana-teiki <92569762+sakana-teiki@users.noreply.github.com>
2021-10-24 00:58:33 +0900

    MIT License表示の追加

--- components/footer.php
+++ components/footer.php
@@ -2,3 +2,4 @@
 </section>
 </body>
 </html>
+<!-- This website contains open source softwares. | Teiki Adventure | MIT License | https://raw.githubusercontent.com/sakana-teiki/teiki-adventure/master/LICENSE -->
