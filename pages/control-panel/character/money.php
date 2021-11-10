b3d1b249a0adc7d4ac47688717f59adbc2eb4a76
sakana-teiki <92569762+sakana-teiki@users.noreply.github.com>
2021-11-11 00:41:20 +0900

    所持金の付与機能

---
+++ pages/control-panel/character/money.php
@@ -0,0 +1,130 @@
+<?php
+  require GETENV('GAME_ROOT').'/middlewares/initialize.php';
+  require GETENV('GAME_ROOT').'/middlewares/verification_administrator.php';
+
+  require_once GETENV('GAME_ROOT').'/utils/validation.php';
+
+  // POSTリクエスト時の処理
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // 入力値検証
+    if (
+      !validatePOST('eno',    ['non-empty', 'natural-number']) ||
+      !validatePOST('number', ['non-empty', 'natural-number'])
+    ) {
+      responseError(400);
+    }
+
+    // 所持金の付与
+    $statement = $GAME_PDO->prepare("
+      UPDATE
+        `characters`
+      SET
+        `money` = `money` + :number
+      WHERE
+        `ENo` = :ENo;
+    ");
+
+    $statement->bindParam(':number', $_POST['number'], PDO::PARAM_INT);
+    $statement->bindParam(':ENo',    $_POST['eno']);
+
+    $result = $statement->execute();
+
+    if (!$result) {
+      responseError(500); // 実行に失敗した場合は500(Internal Server Error)を返して処理を中断
+    }
+  }
+
+  $PAGE_SETTING['TITLE'] = '所持金付与';
+
+?>
+<?php require GETENV('GAME_ROOT').'/components/header.php'; ?>
+<?php require GETENV('GAME_ROOT').'/components/header_end.php'; ?>
+
+<h1>所持金付与</h1>
+
+<?php if ($_SERVER['REQUEST_METHOD'] == 'POST') { ?>
+<section>
+  <h2>実行結果</h2>
+
+  <p>
+    ENo.<?=$_POST['eno']?>に所持金が<?=$_POST['number']?>G付与されました。 
+  </p>
+</section>
+<?php } ?>
+
+<section>
+  <h2>所持金付与</h2>
+
+  <form id="form" method="post">
+    <input type="hidden" name="csrf_token" value="<?=$_SESSION['token']?>">
+    
+    <section class="form">
+      <div class="form-title">ENo</div>
+      <div class="form-description">付与を行うキャラクターのENoを指定します。</div>
+      <input id="input-eno" class="form-input" type="number" name="eno" placeholder="ENo">
+      <button id="check-eno" type="button" class="form-check-button">対象を確認</button>
+    </section>
+
+    <section class="form">
+      <div class="form-title">付与額</div>
+      <div class="form-description">付与する金額を指定します。</div>
+      <input id="input-money" class="form-input" type="number" name="number" placeholder="金額">
+    </section>
+
+    <div id="error-message-area"></div>
+
+    <div class="button-wrapper">
+      <button class="button" type="submit">付与</button>
+    </div>
+  </form>
+</section>
+
+<script>
+  // 送信先を確認ボタンを押したら新しいタブで指定のENoのプロフィールページを表示
+  $('#check-eno').on('click', function(){
+    window.open('<?=$GAME_CONFIG['URI']?>profile?ENo=' + $('#input-eno').val());
+  });
+
+  var waitingResponse = false; // レスポンス待ちかどうか（多重送信防止）
+
+  // エラーメッセージを表示する関数及びその関連処理
+  var errorMessageArea = $('#error-message-area');
+  function showErrorMessage(message) {
+    errorMessageArea.empty();
+    errorMessageArea.append(
+      '<div class="message-banner message-banner-error">'+
+        message +
+      '</div>'
+    );
+  }
+
+  $('#form').submit(function(){
+    // 値を取得
+    var inputENo   = $('#input-eno').val();
+    var inputMoney = $('#input-money').val();
+
+    // 入力値検証
+    // ENoが入力されていない場合エラーメッセージを表示して送信を中断
+    if (!inputENo) {
+      showErrorMessage('ENoが入力されていません');
+      return false;
+    }
+
+    // 付与金額が入力されていない場合エラーメッセージを表示して送信を中断
+    if (!inputMoney) {
+      showErrorMessage('付与額が入力されていません。');
+      return false;
+    }
+
+    // レスポンス待ちの場合アラートを表示して送信を中断
+    if (waitingResponse == true) {
+      alert("送信中です。しばらくお待ち下さい。");
+      return false;
+    }
+
+    // 上記のどれにも当てはまらない場合送信が行われるためレスポンス待ちをONに
+    waitingResponse = true;
+  });
+</script>
+
+<?php require GETENV('GAME_ROOT').'/components/footer.php'; ?>
--- pages/control-panel/index.php
+++ pages/control-panel/index.php
@@ -25,6 +25,7 @@
   <a href="<?=$GAME_CONFIG['URI']?>control-panel/notification">通知送信</a><br>
 
   <h2>キャラクターデータ変更</h2>
+  <a href="<?=$GAME_CONFIG['URI']?>control-panel/character/money">所持金付与</a><br>
   <a href="<?=$GAME_CONFIG['URI']?>control-panel/character/icon-limit">アイコン上限付与</a><br>
   <a href="<?=$GAME_CONFIG['URI']?>control-panel/character/password">パスワード再発行</a><br>
   <a href="<?=$GAME_CONFIG['URI']?>control-panel/character/delete">指定キャラクター削除</a><br>
